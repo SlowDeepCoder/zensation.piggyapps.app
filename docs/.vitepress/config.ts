20381e9dbb1ecd627c145a1ae52ed962adc6dc52
SlowDeepCoder <danel.lindgren@gmail.com>
2024-07-14 16:54:03 +0200

    add icon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,4 +2,7 @@ export default {
   title: 'Zensation',
   description: 'Web page for the Zensation app.',
   themeConfig: {},
+  head: [
+    ['link', { rel: 'icon', href: './public/icon.png' }]
+  ],
 };
